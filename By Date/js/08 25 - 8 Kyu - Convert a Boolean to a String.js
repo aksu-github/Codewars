68c84cad01bb7cde3d4b5f5ff248d6c407c6c7af
Alvin Su <su.alvin@proton.me>
2022-08-25 14:36:43 -0700

    08/25 - Update

---
+++ By Date/js/08 25 - 8 Kyu - Convert a Boolean to a String.js
@@ -0,0 +1,12 @@
+/*
+Instruction:
+
+Implement a function which convert the given boolean value into its string representation.
+
+*/
+
+//  My Solution 
+
+function booleanToString(b){
+  return b.toString();
+}
