eb922f87c50b80dc9acfc69206aaafd0fc1be3c8
Alvin Su <su.alvin@proton.me>
2022-08-27 17:01:05 -0700

    08/27 - Update

---
+++ By Difficulty/8 Kyu/js/08 27 - 8 Kyu - Training JS 1 Create Your First JS Function and Print Hello World.js
@@ -0,0 +1,23 @@
+/*
+Instruction:
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+RNA differs slightly from DNA its chemical structure and contains no Thymine. 
+
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+
+*/
+
+//  My Solution 
+
+function helloWorld(){
+  let str = 'Hello World!';
+  console.log(str);
+  return str;
+}
