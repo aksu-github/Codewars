5c4af1d26d8f2d8341da2c315d52d5a8d6567e5d
Alvin Su <su.alvin@proton.me>
2022-09-30 16:12:37 -0700

    09/30 - Update

---
+++ By Date/js/09 30 - 8 Kyu - Grosshopper Messi Goals Function.js
@@ -0,0 +1,17 @@
+/*
+Instruction:
+
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+*/
+
+//  My Solution
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
