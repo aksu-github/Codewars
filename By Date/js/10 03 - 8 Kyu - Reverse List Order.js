536e90baab9cfbf60acadaef527bd10475d8d96c
Alvin Su <su.alvin@proton.me>
2022-10-03 15:39:11 -0700

    10/03 - Update

---
+++ By Date/js/10 03 - 8 Kyu - Reverse List Order.js
@@ -0,0 +1,11 @@
+/*
+Instruction:
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+*/
+
+//  My Solution
+
+function reverseList(list) {
+  return list.reverse();
+}
