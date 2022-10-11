e39db19f6f856de1a69b508ec661af3f1b9d937a
Alvin Su <su.alvin@proton.me>
2022-10-10 17:52:25 -0700

    10/10 - Update

---
+++ By Difficulty/8 Kyu/js/10 10 - 8 Kyu - Keep Up the Hoop.js
@@ -0,0 +1,13 @@
+/*
+Instruction:
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+*/
+
+//  My Solution
+
+function hoopCount (n) {
+  return n > 9 ? `Great, now move on to tricks` : `Keep at it until you get it`;
+}
