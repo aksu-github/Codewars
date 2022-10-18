aff86a0da32c825d479b46265acf40bb1d274186
Alvin Su <su.alvin@proton.me>
2022-10-18 11:02:21 -0700

    10/18 - Update 1

---
+++ By Date/js/10 18 - 8 Kyu - Training JS 5 Basic Data Types Object.js
@@ -0,0 +1,14 @@
+/*
+Instruction:
+
+Complete the function that takes two integers (a, b, where a < b) 
+and return an array of all integers between the input parameters, 
+including them.
+
+*/
+
+//  My Solution
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
