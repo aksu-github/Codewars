9ef8fc68f413e373a206f9580a491322fc4ef9a1
Alvin Su <su.alvin@proton.me>
2022-11-02 23:50:46 -0700

    11/02 - Update 2

---
+++ By Difficulty/7 Kyu/js/11 02 - 7 Kyu - Remove the Minimum.js
@@ -0,0 +1,23 @@
+/*
+Instruction:
+
+Given an array of integers, remove the smallest value. 
+
+Do not mutate the original array/list. 
+If there are multiple elements with the same value, 
+remove the one with a lower index. 
+
+If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+*/
+
+//  My Solution 
+
+function removeSmallest(numbers) {
+  
+  let minIndex = numbers.indexOf(Math.min(...numbers));
+  
+  return numbers.filter( (_,i) => i != minIndex );
+}
