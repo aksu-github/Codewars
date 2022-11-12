fb38b6a2f4afad50d4240f6568a32d7509fe6b5a
Alvin Su <su.alvin@proton.me>
2022-11-11 16:21:48 -0800

    11/11- Update 1

---
+++ By Date/js/11 11 - 8 Kyu - Do You Speak English.js
@@ -0,0 +1,25 @@
+/*
+Instruction:
+
+Given a string of arbitrary length with any ascii characters. 
+Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+*/
+
+//  My Solution 
+
+function spEng(sentence){
+  let regex = /english/i;
+  return sentence.match(regex) ? true : false;
+}
+
+function spEng(sentence){
+  let regex = /english/i;
+  return regex.test(sentence);
+}
