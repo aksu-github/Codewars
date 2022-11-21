442d6e234a28259cc3f20e1f549b829a98a76eff
Alvin Su <su.alvin@proton.me>
2022-11-20 18:12:55 -0800

    11/20 - Update 1

---
+++ By Date/js/11 20 - 8 kyu - Simple Validation of Username With Regex.js
@@ -0,0 +1,18 @@
+/*
+Instruction:
+
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+
+*/
+
+//  My Solution 
+
+function validateUsr(username) {
+  let res =  /^[a-z\d_]{4,16}$/g.test(username) 
+  return res;
+}
