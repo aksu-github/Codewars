dab9dba7c5cce28d0985ac04708481f212a30aa8
Alvin Su <su.alvin@proton.me>
2022-11-24 23:00:04 -0800

    11/24 - Update 1

---
+++ By Date/js/11 24 - 8 Kyu - Grasshopper Variable Assignment Debug.js
@@ -0,0 +1,13 @@
+/*
+Instruction:
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+*/
+
+//  My Solution 
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
