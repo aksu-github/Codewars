640992917e95a218ef8a93e301de644ba273df2e
Alvin Su <su.alvin@proton.me>
2022-12-12 15:08:06 -0800

    12/12 - Update 1

---
+++ By Date/js/12 12 - 8 Kyu - Grasshopper Messi Goals.js
@@ -0,0 +1,12 @@
+/*
+Instruction:
+
+*/
+
+//  My Solution 
+
+var laLigaGoals = 43,
+    championsLeagueGoals = 10,
+    copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
