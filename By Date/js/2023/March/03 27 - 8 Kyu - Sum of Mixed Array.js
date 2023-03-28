627d31c8b5604140017dbddf3cb4190fa638b35e
Alvin Su <su.alvin@proton.me>
2023-03-27 22:31:15 -0700

    2023_03_27 - Update 1

---
+++ By Date/js/2023/March/03 27 - 8 Kyu - Sum of Mixed Array.js
@@ -0,0 +1,14 @@
+/*
+Instruction:
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+*/
+
+//  My Solution
+
+function sumMix(x){
+  return x.reduce( (a,b) => +a + +b, 0);
+}
